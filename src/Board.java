3040dccb7e164fd1cd164a1f9ce377a347d6ee8b
Junqiang Qian <junqiang.qian@hotmail.co.uk>
2017-01-18 15:54:29 +0000

    RSetup TicTacToe class and add display method to board

--- src/Board.java
+++ src/Board.java
@@ -2,4 +2,20 @@
  * Created by jq on 18/01/17.
  */
 public class Board {
+
+    private char[][] board;
+
+    public Board(final int boardSize) {
+        this.board = new char[boardSize][boardSize];
+    }
+
+    public void displayBoard() {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
 }
--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,6 +1,68 @@
+import java.util.Scanner;
+
 public class TicTacToe {
 
-    public static void main(String[] args) {
-        System.out.println("Hello world");
+    static final int BOARD_SIZE = 3;
+
+    private Board board;
+    private Player player1;
+    private Player player2;
+
+    public TicTacToe(Board board) {
+        this.board = board;
     }
+
+    public void play() {
+       start();
+    }
+
+    private void start() {
+        System.out.println("Welcome to TicTacToe, please enter which mode which you would like to play");
+        Scanner scanner = new Scanner(System.in);
+        int mode;
+        do {
+            System.out.println("0 - Human vs Human, 1 - Human vs Computer, 2 - Computer vs Computer");
+            while (!scanner.hasNextInt()) {
+                System.out.println("Error, not a number, please try again");
+                scanner.next();
+            }
+            mode = scanner.nextInt();
+        } while (mode < 0 || mode > 2);
+        playMode(mode);
+    }
+
+    private void playMode(int mode) {
+        switch(mode) {
+            case 0:
+                humanVsHuman();
+                break;
+            case 1:
+                humanVsComputer();
+                break;
+            case 2:
+                computerVsComputer();
+                break;
+            default:
+                System.out.println("Error, invalid mode");
+        }
+    }
+
+    private void humanVsHuman() {
+        this.player1 = new Human('x');
+        this.player2 = new Human('o');
+        System.out.println("Human vs Human Mode");
+    }
+
+    private void humanVsComputer() {
+        this.player1 = new Human('x');
+        this.player2 = new Computer('o');
+        System.out.println("Human vs Computer Mode");
+    }
+
+    private void computerVsComputer() {
+        this.player1 = new Computer('x');
+        this.player2 = new Computer('o');
+        System.out.println("Computer vs Computer Mode");
+    }
+
 }
